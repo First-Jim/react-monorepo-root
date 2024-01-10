9844f34203d322b961f5f422ac6e95ece4dc0bfb
Jimmy Liu <jmliu@yuansuan.cn>
2024-01-10 17:02:07 +0800

    yarn workspace init

--- packages/app1/src/main.tsx
+++ packages/app1/src/main.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
 import { App1 } from "./App1";
-ReactDOM.createRoot(document.getElementById("app1")!).render(<App1 />);
+// ReactDOM.createRoot(document.getElementById("app1")!).render(<App1 />);
 
 export { App1 } from "./App1";
